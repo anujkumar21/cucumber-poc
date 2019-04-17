5d626248dbd8b7bbd6a790a82fa6d04b8e850b95
Anuj Kumar <cdac.anuj@gmail.com>
2019-04-17 13:09:15 +0530

    Added testng way to run the test.

--- src/test/java/com/poc/runners/MainRunner.java
+++ src/test/java/com/poc/runners/MainRunner.java
@@ -4,17 +4,19 @@ import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
 
+// @formatter:off
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		features = {"src\\test\\resources"},
-		glue = {"com.poc.stepDefinitions"},
-		monochrome = true,
-		tags = {},
-		plugin = {"pretty", "html:target/cucumber", "com.cucumber.listener.ExtentCucumberFormatter:target/report.html"}
-		)
-public class MainRunner {
+@CucumberOptions
+	(features = { "src\\test\\resources" }, 
+	glue = {"com.poc.stepDefinitions" }, 
+	monochrome = true, 
+	tags = {}, 
+	plugin = { "pretty", "html:target/cucumber"})
 
-	
+//@formatter:on
+public class MainRunner extends AbstractTestNGCucumberTests {
+
 }
